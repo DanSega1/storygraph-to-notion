29ce30170f6bd2fc1341e775b39cc406afdf29ac
Henry Desroches <yo@henry.codes>
2024-12-05 16:58:06 -0700

    🎉 - Init commit

---
+++ functions/currentlyReading.js
@@ -0,0 +1,30 @@
+import "dotenv/config";
+
+const USERNAME = process.env.USERNAME;
+
+export const handler = async (req) => {
+  const username = req.queryStringParameters?.username || USERNAME;
+
+  try {
+    const responseCurrentlyReading = await fetch(
+      `${process.env.URL}/.netlify/functions/getList?target=currently-reading&username=${username}`,
+      { signal: AbortSignal.timeout(26000) }
+    );
+
+    const currentlyReading = await responseCurrentlyReading.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(currentlyReading),
+    };
+  } catch (error) {
+    console.error("Error:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error }),
+    };
+  }
+};
